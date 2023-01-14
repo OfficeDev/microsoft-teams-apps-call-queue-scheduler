13975a89cc0995c88b3829b84d01d0720fbfbad8
Srinivas Varukala <svarukala@gmail.com>
2023-01-13 23:24:43 -0500

    updated spo deploy to use pnp ps only

--- Deployment/deploy-spo.ps1
+++ Deployment/deploy-spo.ps1
@@ -1,74 +1,39 @@
 Param (
     [parameter(mandatory = $true)] [string]$SPOSiteTitle,  # Title of the SharePoint site to be created
-    [parameter(mandatory = $true)] [string]$AzFunctionMID # Azure Function Managed Identity ID
+    [parameter(mandatory = $true)] [string]$AzFunctionMID, # Azure Function Managed Identity ID
+    [parameter(mandatory = $true)] [string]$SPOHostUrl # Host url for the SPO site e.x: "moderncomms996974.sharepoint.com"
 )
+Connect-PnPOnline -Url $SPOHostUrl -DeviceLogin
+<#
+$ctx = Get-PnPContext
+$ctx.Load($ctx.Web.CurrentUser)
+$ctx.ExecuteQuery()
+$userEmail = $ctx.Web.CurrentUser.Email
+#>
 
-Import-Module Microsoft.Graph.Sites
-Connect-MgGraph -Scope Sites.FullControl.All, User.Read.All, Group.ReadWrite.All -DeviceCode
+# Creating SPO Site
+Write-Host "Provisioning SPO site...";
+$site = New-PnPSite -Type TeamSite -Title $SPOSiteTitle -Alias $SPOSiteTitle.Replace(' ','') -Description "SPO site used as backend for the Call Queue Scheduler app" -Wait
+Write-Host "SPO site created: $site";
+Write-Host ""
 
-#Sample MID used for testing
-#$AzureFunctionMID = "5938d428-1311-4d66-8727-057b906ae7fc"
-
-$uri = "https://graph.microsoft.com/v1.0/me"
-$userId = (Invoke-MgGraphRequest -Method Get -Uri $uri).id
-
-#Create M365 group using Microsoft Graph PowerShell
-$Owner = "https://graph.microsoft.com/v1.0/users/$userId"
-$params = @{
-	Description = "SPO site used as backend for the Call Queue Scheduler app"
-	DisplayName = $SPOSiteTitle
-	GroupTypes = @(
-        "Unified"
-	)
-	MailEnabled = $false
-	MailNickname = $SPOSiteTitle
-	SecurityEnabled = $true
-	"Owners@odata.bind" = @(
-		$Owner
-	)
-}
-$group = New-MgGroup -BodyParameter $params
-$groupId = $group.Id
-$Uri= "https://graph.microsoft.com/v1.0/groups/$groupId/sites/root"
-$siteInfo = $null
-Do {
-    # Pause for 5 seconds per loop
-    Start-Sleep -s 5
-    # Check if the site has been created
-    try
-    {
-        $siteInfo = Invoke-MgGraphRequest -Method Get -Uri $Uri
-        Write-Host "SPO site created."
-    }
-    catch
-    {
-        $siteInfo = $null
-        Write-Host "SPO site is still provisioning. Retry in 5 seconds..."
-    }
-}
-while ($siteInfo -eq $null)
-
+# Granting write permissions to the Azure Function Managed Identity using Site.Selected scope
 Write-Host "Granting write permissions to the Azure Function Managed Identity using Site.Selected scope"
-$application = @{
-    id = $AzFunctionMID
-    displayName = "cqs-azfunction-mid"
-}
-$appRole = "write"
-New-MgSitePermission -SiteId $siteInfo.id -Roles $appRole -GrantedToIdentities @{ Application = $application }
+Grant-PnPAzureADAppSitePermission -AppId $AzFunctionMID -DisplayName "cqs-azfunction-mid" -Permissions Write -Site $site
+Write-Host ""
 
 # Deploy the site template
 Write-Host "Deploying site template to create the required lists and libraries"
-Connect-PnPOnline -Url $siteInfo.webUrl -DeviceLogin
-Invoke-PnPSiteTemplate -Path ..\Pkgs\shift-manager-site-lists.xml -Verbose -IgnoreDuplicateDataRowErrors -Handlers Lists
+Connect-PnPOnline -Url $site -DeviceLogin
+Invoke-PnPSiteTemplate -Path ..\Pkgs\cqs-manager-site-lists.xml -Verbose -IgnoreDuplicateDataRowErrors -Handlers Lists
+$RootWeb = Get-PnPSite -Includes ID
+$Web = Get-PnPWeb -Includes ID
+Write-Host ""
 
 Write-Host -ForegroundColor magenta "Here is the information you'll need to deploy and configure while following the remaining steps"
+Write-Host "SPO Site Url: " $site
+Write-Host "SPO Site ID: $SPOHostUrl,{$RootWeb.Id},{$Web.Id}"
+Get-PnPList | ?{$_.Title -like "Shifts*"} | FT
 
-Write-Host "SPO Site Url: " $siteInfo.webUrl
-Write-Host "SPO Site ID: " $siteInfo.Id
-Get-MgSiteList -SiteId $siteInfo.Id | ?{$_.Name -like "Shifts*"} | Select DisplayName, Id, WebUrl
-
-
-
-# Disconnecting sessions
-disconnect-MgGraph
+# Disconnecting session
 Disconnect-PnPOnline
